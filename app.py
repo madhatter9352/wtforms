2006462c8e0545d3c68f2df6c781665a8b00e0a3
lazaro <lachy9352@gmail.com>
2020-11-07 16:30:37 -0400

    add validation form

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Mysecret!'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(),
+                                                   Length(min = 4, max = 8, message='Must be between 4 and 8 characters')])
+    password = PasswordField('password', validators=[InputRequired()])
+
+
+@app.route('/', methods =['GET', 'POST'])
+def index():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>User: {}, Password: {}'.format(form.username.data, form.password.data)
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
